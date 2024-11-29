e23beb75538572d1b9d7e5c51c9f0125bef18170
exochron <exochron@freenet.de>
2024-11-29 20:15:06 +0100

    chore(toc): updated interface version

--- Tests/example.lua
+++ Tests/example.lua
@@ -2,12 +2,14 @@ function GG_Test_Example()
     local gg = LibStub("GalleryGenerator")
     gg:TakeScreenshots(
             {
+                -- First shot with open character frame
                 function(api)
                     api:BackScreen() -- hide game world with black screen
 
                     ToggleCharacter("PaperDollFrame") -- show character frame
                     api:PointAndClick(PaperDollFrame.ExpandButton)
                 end,
+                -- second shot with reputation frame
                 function(api)
                     api:BackScreen(0, 1, 0) -- green screen
                     api:Click(PaperDollFrame.ExpandButton) -- revert previous toggle
@@ -15,9 +17,10 @@ function GG_Test_Example()
                     api:PointAndClick(CharacterFrameTab2)
                 end,
             },
+            -- cleanup and revert previous states
             function(api)
                 api:Click(CharacterFrameTab1)
-                ToggleCharacter("PaperDollFrame")
+                ToggleCharacter("PaperDollFrame") -- hide character frame
             end
     )
 end
